b942bd5530a18c52b28d388424789c1e46c04067
vosamo <vosamo@outlook.com>
2016-10-15 13:55:41 +0800

    获取token，渲染vnc_auto登录

--- novnc/views.py
+++ novnc/views.py
@@ -8,13 +8,14 @@ def run_vnc(request):
 
     # token = token  # Used to determine the different access machine
     view_only = request.GET.get('view_only', 'false') # False can control the or true can only view
+    token = request.GET.get('token')
 
     # The proxy server IP and port, this usually use school server LAN IP (127.0.0.1, 6080 is the default port)
     host = '127.0.0.1'
     port = settings.VNC_PROXY_PORT
 
     # context = Context({'token':token, 'host':host, 'port': port, 'view_only':view_only})
-    context_dict = {'token': 'abcdefg', 'host': host, 'port': port, 'view_only': view_only}
+    context_dict = {'token':token, 'host': host, 'port': port, 'view_only': view_only}
 
-    # return render(request, 'vnc_auto.html', context_dict)
-    return render(request, 'vnc.html')
+    return render(request, 'vnc_auto.html', context_dict)
+    # return render(request, 'vnc.html')
